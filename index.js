e2f24ee661b854816af128bd69a290138cb975e6
Dave Kokandy <drkokandy@gmail.com>
2017-11-10 05:44:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks() {
+  
+}
+
+function calculateVertical(a,b) {
+  if (a >= b) {
+    return (a - b)*264
+  }
+  else if (b > a) {
+    return (b - a)*264
+    }
+}
